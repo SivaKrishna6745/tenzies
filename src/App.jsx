7b3b1518ab2afa394f18a9e19722b1df70a0ed20
SIVAKRISHNA <krishnasiva6745@gmail.com>
2025-04-22 12:54:16 +0530

    - add method for hold a die. - add styling for held dice. - update rollDice method to roll only unheld dice

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,12 @@ function App() {
     function generateAllNewDice() {
         const diceNumbers = [];
         for (let i = 0; i < 10; i++) {
-            const randomNumber = Math.floor(Math.random() * 6) + 1;
-            diceNumbers.push({ id: i, value: randomNumber, isHeld: false });
+            const randomNumber = Math.ceil(Math.random() * 6);
+            diceNumbers.push({
+                id: i,
+                value: randomNumber,
+                isHeld: false,
+            });
         }
         return diceNumbers;
     }
@@ -15,21 +19,32 @@ function App() {
     const [dice, setDice] = useState(generateAllNewDice());
 
     function rollDice() {
-        setDice((prevDice) => generateAllNewDice());
+        setDice((prevDice) =>
+            prevDice.map((eachDie) => (eachDie.isHeld ? eachDie : { ...eachDie, value: Math.ceil(Math.random() * 6) }))
+        );
+    }
+
+    function hold(id) {
+        setDice((prevDice) =>
+            prevDice.map((eachDie) => (eachDie.id === id ? { ...eachDie, isHeld: !eachDie.isHeld } : eachDie))
+        );
     }
     return (
-        <>
-            <main className="flex fl-cntr">
-                <div className="container grid">
-                    {dice.map((dieObject) => (
-                        <Die key={dieObject.id} value={dieObject.value} isHeld={dieObject.isHeld} />
-                    ))}
-                </div>
-                <button type="button" onClick={rollDice} className="roll-dice">
-                    Roll
-                </button>
-            </main>
-        </>
+        <main className="flex fl-cntr">
+            <div className="container grid">
+                {dice.map((dieObject) => (
+                    <Die
+                        key={dieObject.id}
+                        value={dieObject.value}
+                        isHeld={dieObject.isHeld}
+                        hold={() => hold(dieObject.id)}
+                    />
+                ))}
+            </div>
+            <button type="button" onClick={rollDice} className="roll-dice">
+                Roll
+            </button>
+        </main>
     );
 }
 
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-function Die({ value, isHeld }) {
+function Die({ value, isHeld, hold }) {
     return (
-        <button type="button" className={`die ${isHeld ? 'held' : ''}`}>
+        <button type="button" className={`die ${isHeld ? 'held' : ''}`} onClick={hold}>
             {value}
         </button>
     );
